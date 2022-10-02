8d8b60f73309c4036a94768677d43aa115a481e1
unknown <rjhjuu@gmail.com>
2022-10-02 11:39:17 +0300

    calculate standardized mortality ratio

--- deaths.R
+++ deaths.R
@@ -4,6 +4,7 @@ library(geofi)
 library(pxweb)
 library(scales)
 library(lubridate)
+library(data.table)
 
 # Which years had the most deaths? -------------------------------------------- 
 px_12at <- pxweb_get(url = 
@@ -67,7 +68,7 @@ plot_annotation(title = "Yearly deaths, population and births in Finland, 1749 -
 caption = "source: Tilastokeskus 12at -- Vital statistics and population, 1749-2021"
 ) -> plot_yearly
 
-ggsave("images//plot_yearly.png", plot_yearly, device = "png", dpi = 96,
+ggsave("images//1_plot_yearly.png", plot_yearly, device = "png", dpi = 96,
        width = 9, height = 9, units = c("in"))
 
 # Which months have the most deaths? -------------------------------------
@@ -98,7 +99,7 @@ df_12ah |>
   ggplot(aes(Month, Deaths_daily)) +
   geom_col() +
   geom_text(aes(label = Deaths_daily), vjust = 1.5,
-            color = "white", size = 3) +
+            color = "white", size = 4) +
   theme_bw() +
   labs(y = NULL,
        x = NULL) -> daily_deaths_month_plot
@@ -112,20 +113,142 @@ df_12ah |>
   theme_bw() +
   facet_wrap(vars(Decade), nrow = 2) +
   geom_text(aes(label = Deaths_daily),
-            color = "white", size = 2.2, angle = 270, hjust = -0.1) +
+            color = "white", size = 3.2, angle = 270, hjust = -0.1) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.15)) +
   labs(subtitle = "By decade",
        y = NULL,
        x = NULL,
-       caption = "source: Tilastokeskus 12ah -- Kuolleet kuukausittain, 1945-2021"
+       caption = "source: Tilastokeskus 12ah -- Deaths by month, 1945-2021"
        ) -> daily_deaths_decade_plot
 
 plot_months <-  daily_deaths_month_plot / daily_deaths_decade_plot +
   plot_annotation(title = "Daily deaths in Finland by month (1945-2021)",
                   subtitle = "Death rates are higher in winter months")
 
-ggsave("images//plot_months.png", plot_months, device = "png", dpi = 96,
+ggsave("images//2_plot_months.png", plot_months, device = "png", dpi = 96,
        width = 9, height = 9, units = c("in"))
+
+
+# ## Standardization ----------------------------------------------
+
+# Get classifications
+# Source:
+# The correspondence table between municipalities and regions in 2020
+# https://www.stat.fi/en/luokitukset/corrmaps/kunta_1_20200101%23maakunta_1_20200101/
+
+region_keys_2020 <- fread("kunta_1_20200101%23maakunta_1_20200101.csv", encoding = "Latin-1",
+                          col.names = c("id_1", "Municipality", "id_2", "Region"), header = F)
+
+
+# Get populations and ages per region per year
+
+px_11rf <- pxweb_get(url = 
+"https://statfin.stat.fi:443/PxWeb/api/v1/en/StatFin/vaerak/statfin_vaerak_pxt_11rf.px",
+query = list(
+"Kunta" = c("*"),
+"Sukupuoli" = c("1", "2"),
+"Vuosi" = c("2016", "2017", "2018", "2019", "2020"),
+"Ikä" = c("*"),
+"Tiedot" = c("*")))
+
+df_11rf <- as_tibble(as.data.frame(px_11rf, column.name.type = "text", variable.value.type = "text"))
+
+df_11rf |>
+  drop_na() |>
+  filter(Municipality != "WHOLE COUNTRY",
+         Age != "Total") |>
+  left_join(region_keys_2020, by = "Municipality") |>
+  mutate(Age = ifelse(Age == "100 -", "100", Age),
+         Age = as.integer(Age)) |>
+  mutate(Age_group = case_when(Age == 0 ~ "0",
+                               between(Age, 1, 4) ~ "1 - 4",
+                               between(Age, 5, 9) ~ "5 - 9",
+                               between(Age, 10, 14) ~ "10 - 14",
+                               between(Age, 15, 19) ~ "15 - 19",
+                               between(Age, 20, 24) ~ "20 - 24",
+                               between(Age, 25, 29) ~ "25 - 29",
+                               between(Age, 30, 34) ~ "30 - 34",
+                               between(Age, 35, 39) ~ "35 - 39",
+                               between(Age, 40, 44) ~ "40 - 44",
+                               between(Age, 45, 49) ~ "45 - 49",
+                               between(Age, 50, 54) ~ "50 - 54",
+                               between(Age, 55, 59) ~ "55 - 59",
+                               between(Age, 60, 64) ~ "60 - 64",
+                               between(Age, 65, 69) ~ "65 - 69",
+                               between(Age, 70, 74) ~ "70 - 74",
+                               between(Age, 75, 79) ~ "75 - 79",
+                               between(Age, 80, 84) ~ "80 - 84",
+                               between(Age, 85, 89) ~ "85 - 89",
+                               between(Age, 90, 94) ~ "90 - 94",
+                               between(Age, 95, 150) ~ "95 -",)) |>
+  rename(Population = "Population 31 Dec") |>
+  select(Municipality, Year, Sex, Population, Region, Age_group) |>
+  rename(Age = Age_group) |>
+  mutate(Region = case_when(Municipality == "Juankoski" ~ "North Savo",
+                              Municipality == "Luvia" ~ "Satakunta",
+                              Municipality == "Valtimo" ~ "North Karelia",
+                              TRUE ~ Region)) |>
+  group_by(Region, Year, Age, Sex) |>
+  summarise(Population = sum(Population)) -> df_11rf
+
+df_11rf |>
+  group_by(Year, Age, Sex) |>
+  summarise(Population_All_Regions = sum(Population)) -> df_all_regions
+
+df_11rf |>
+  left_join(df_all_regions, by = c("Year", "Age", "Sex")) -> df_11rf
+
+# Get Deaths by Underlying cause of death (time series classification), Age, Information, Gender and Year
+
+px_11bs <- pxweb_get(url = 
+"https://statfin.stat.fi:443/PxWeb/api/v1/en/StatFin/ksyyt/statfin_ksyyt_pxt_11bs.px",
+query = list(
+"Tilaston peruskuolemansyy (aikasarjaluokitus)" = c("*"),
+"Sukupuoli" = c("1", "2"),
+"Vuosi" = c("2016", "2017", "2018", "2019", "2020"),
+"Ikä" = c("*"),
+"Tiedot" = c("*")))
+
+df_11bs <- as_tibble(as.data.frame(px_11bs, column.name.type = "text", variable.value.type = "text"))
+
+df_11bs |>
+  rename(Cause = "Underlying cause of death (time series classification)",
+         Sex = Gender) |>
+  filter(Age != "Total") -> df_11bs
+
+df_11rf |>
+  left_join(df_11bs, by = c("Age", "Sex", "Year")) |>
+  rename(Deaths_All_Regions = Deaths) |>
+  mutate(Expected_deaths = (Population / Population_All_Regions) * Deaths_All_Regions) |>
+  group_by(Region, Cause) |>
+  summarise(Expected_deaths = sum(Expected_deaths)) -> df_11rf
+
+# For causes of death per region
+
+px_11bt <- pxweb_get(url = 
+ "https://statfin.stat.fi:443/PxWeb/api/v1/en/StatFin/ksyyt/statfin_ksyyt_pxt_11bt.px",
+query = list(
+ "Maakunta" = c("MK01", "MK02", "MK04", "MK05", "MK06", "MK07", "MK08", "MK09", "MK10", 
+                "MK11", "MK12", "MK13", "MK14", "MK15", "MK16", "MK17", "MK18", "MK19", "MK21"),
+ "Tilaston peruskuolemansyy (aikasarjaluokitus)" = c("*"),
+ "Vuosi" = c("2016", "2017", "2018", "2019", "2020"),
+ "Tiedot" = c("*")))
+
+df_11bt <- as_tibble(as.data.frame(px_11bt, column.name.type = "text", variable.value.type = "text"))
+
+df_11bt |>
+  rename(Cause = "Underlying cause of death (time series classification)") |>
+  mutate(Year = as.integer(Year)) |>
+  separate(Region, into = c("ID", "Region"), sep = " ", extra = "merge") |>
+  group_by(Region, Cause) |>
+  summarise(Deaths = sum(Deaths)) -> df_11bt
+
+df_11rf |>
+  left_join(df_11bt, by = c("Region", "Cause")) |>
+  mutate(SMR = Deaths / Expected_deaths) |> # Standardized mortality ratio
+  relocate(Deaths, .before = Expected_deaths) -> df_smr
+
+
 
 # Causes of death per region --------------------------------------------------
 
@@ -181,16 +304,17 @@ df_cause_region <- df_region_map |>
 by_cause <- function(cause_1, title_1){
 
   df_cause_region |>
-    filter(Cause == cause_1) |>
-    ggplot() + 
+    filter(Cause == cause_1) -> df
+    ggplot(df) + 
     geom_sf(aes(geometry = geom, fill = Deaths_per_100k)) +
-    scale_fill_distiller(palette = "Spectral") +
+    scale_fill_distiller(palette = "Spectral",
+                         n.breaks = 6) +
     labs(subtitle = title_1, fill = NULL) +
-    theme(legend.position = c(0.18, 0.61),
+    theme(legend.position = c(0.2, 0.65),
           legend.background = element_blank(),
-          legend.key.size = unit(0.18, "in"),
-          legend.text = element_text(size = 7),
-          plot.subtitle = element_text(size = 8))
+          legend.key.size = unit(0.2, "in"),
+          legend.text = element_text(size = 9),
+          plot.subtitle = element_text(size = 11))
 }
 
 p1 <- by_cause("00-54 Total",
@@ -205,7 +329,7 @@ p4 <- by_cause("25 Dementia, Alzheimers disease (F01, F03, G30, R54)",
 p5 <- by_cause("27-30 Diseases of the circulatory system excl. alcohol-related (I00-I425, I427-I99)",
                "Diseases of the circulatory\nsystem excl. alcohol-related")
 p6 <- by_cause("31-35 Diseases of the respiratory system (J00-J64, J66-J99)",
-               "Diseases of the respiratory system")
+               "Diseases of the\nrespiratory system")
 
 p7 <- by_cause("41 Alcohol-related diseases and accidental poisoning by alcohol",
                "Alcohol-related diseases and\naccidental alcohol poisonings")
@@ -221,6 +345,10 @@ p11 <- by_cause("51 Assault (X85-Y09, Y871)",
 p12 <- by_cause("54 No death certificate",
                 "No death certificate")
 
+s = "11 Malignant neoplasm of larynx, trachea, bronchus and lung (C32-C34)"
+by_cause(s,
+         " ")
+
 plot_regions_1 <- ((p1 | p2 | p3) /  
                      (p4 | p5 | p6)) + 
   plot_annotation(title = "Total and diseases related deaths",
@@ -235,8 +363,10 @@ plot_regions_2 <- (p7 | p8 | p9) /
                                     "by cause of death and region per 100,000 inhabitants"),
                   caption = "source: Tilastokeskus 11bt -- Deaths by underlying cause")
 
-ggsave("images//plot_regions_1.png", plot_regions_1, device = "png", dpi = 130)
-ggsave("images//plot_regions_2.png", plot_regions_2, device = "png", dpi = 130)
+ggsave("images//3_plot_regions_1.png", plot_regions_1, device = "png", dpi = 96,
+       width = 9, height = 9, units = c("in"))
+ggsave("images//3_plot_regions_2.png", plot_regions_2, device = "png", dpi = 96,
+       width = 9, height = 9, units = c("in"))
 
 # Life expectancy at birth---------------- -----------------------------------
 
